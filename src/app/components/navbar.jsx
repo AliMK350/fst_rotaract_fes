93278cc9877224ed6dd2efbeb4ab988d0c50e642
Ali Mejber <alimejber055@gmail.com>
2025-09-28 14:06:46 +0100

    final version 2

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -28,16 +28,13 @@ const Navbar = () => {
 
         {/* Toggler Button */}
         <button
-  className="navbar-toggler me-4"
-  type="button"
-  data-bs-toggle="collapse"
-  data-bs-target="#navbarCollapse"
-  aria-controls="navbarCollapse"
-  aria-expanded="false"
-  aria-label="Toggle navigation"
->
-  <span className="hamburger"></span>
-</button>
+          type="button"
+          className="navbar-toggler me-4"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
         {/* Navigation Links */}
         <div className="collapse navbar-collapse p-3" id="navbarCollapse">
